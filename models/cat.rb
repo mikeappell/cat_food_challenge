ae038a0e1a1f56f33137a4d0de57da07a1f766de
Michael Alpert-Appell <mikeappell@gmail.com>
2018-03-26 11:33:32 -0400

    Add #breed method to Cat
    I'm initially using this for debugging, but might
    end up keeping it - time will tell.

--- models/cat.rb
+++ models/cat.rb
@@ -4,6 +4,11 @@ class Cat
     raise 'Error: negative ages make me a sad kitty :(' if @age < 0
   end
 
+  # TODO: Is this worth creating?
+  def breed
+    self.class.name
+  end
+
   def food_per_week
     raise "Error: 'Cat' should never be instantiated; please create one of its breeds instead"
   end
